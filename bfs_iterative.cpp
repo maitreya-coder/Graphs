6dc8520e18387059f213cd866dd8f092c05df987
Tanishq Yadav <strictlyforcompetitivecoding@gmail.com>
2025-12-08 15:19:28 +0530

    Create bfs_iterative.cpp

---
+++ bfs_iterative.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+void addEdge(vector<vector<int>> &graph, int u, int v){
+    graph[u].push_back(v);
+    graph[v].push_back(u);
+}
+int main(){
+    int n=3;
+    vector<vector<int>> graph(n);
+    addEdge(graph,0,1);
+    addEdge(graph,0,2);
+    vector<bool> visited(n,false);
+    for(int i=0;i<n;i++){
+    if(!visited[i]){
+    queue<int> q;
+    q.push(i);
+    while(!q.empty()){
+        int u = q.front();
+        q.pop();
+        if(visited[u]) continue;
+        cout<<u<<"\n";
+        visited[u] = true;
+        for(int j=0;j<graph[u].size();j++){
+            q.push(graph[u][j]);
+        } 
+    }
+    }
+    }
+    return 0;
+}
